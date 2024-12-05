aa7b6f8fb95f58e6a3c4592e2c8adb09763affda
Behon Baker <53905713+BayBreezy@users.noreply.github.com>
2024-12-05 16:09:13 -0500

    style: format codebase

--- app/utils/auth-client.ts
+++ app/utils/auth-client.ts
@@ -1,5 +1,5 @@
-import { createAuthClient } from "better-auth/vue";
 import { adminClient } from "better-auth/client/plugins";
+import { createAuthClient } from "better-auth/vue";
 
 export const authClient = createAuthClient({
   plugins: [adminClient()],
